cab60605ae8bb2d97b4e92a6b80447f54f72c345
Rob Kreisel <rob.kreisel@jedunn.com>
2020-04-26 18:16:10 -0500

    Adding a transaction entity

---
+++ ForwardBalance.API/Entities/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForwardBalance.API.Entities
+{
+    public class Transaction
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Description { get; set; }
+        [Required]
+        public float Amount { get; set; }
+        [ForeignKey("AccountId")]
+        public Account Account { get; set; }
+        public int AccountId { get; set; }
+        [ForeignKey("AccountId")]
+        public Account TransferAccount { get; set; }
+        public int TransferAccountId { get; set; }
+    }
+}
---
+++ ForwardBalance.API/Models/TransactionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForwardBalance.API.Models
+{
+    public class TransactionDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public float Amount { get; set; }
+       
+
+    }
+}
